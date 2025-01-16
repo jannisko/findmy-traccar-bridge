4aec5b529f18d9f8dcc7768ae9fe792dd7bd2307
Jannis Kowalick <jannisko99@gmail.com>
2025-01-16 20:04:22 +0100

    format

--- findmy_traccar_bridge/bridge.py
+++ findmy_traccar_bridge/bridge.py
@@ -33,11 +33,13 @@ persistent_data_store = Path("/data/persistent_data.json")
 acc_store = Path("/data/account.json")
 acc = AppleAccount(RemoteAnisetteProvider(ANISETTE_SERVER))
 
+
 class Location(TypedDict):
     id: int
     timestamp: int
     lat: float
     lon: float
+
 
 class PersistentData(TypedDict):
     # rejected locations by traccar (id has not been claimed by a user), will keep retrying to upload these
@@ -47,14 +49,18 @@ class PersistentData(TypedDict):
     # unix timestamp
     last_apple_api_call: int
 
+
 if not persistent_data_store.is_file():
     persistent_data_store.write_text(
-        json.dumps(PersistentData(
-            pending_locations=[],
-            uploaded_locations=[],
-            last_apple_api_call=0,
-        ))
+        json.dumps(
+            PersistentData(
+                pending_locations=[],
+                uploaded_locations=[],
+                last_apple_api_call=0,
+            )
+        )
     )
+
 
 def bridge() -> None:
     """
@@ -71,7 +77,7 @@ def bridge() -> None:
         logging.info(
             "Login token file not found at '%s'. You must first generate it interactively via "
             "`docker compose exec bridge /bridge/.venv/bin/findmy-traccar-bridge-init`",
-            str(acc_store)
+            str(acc_store),
         )
         while not acc_store.is_file():
             time.sleep(1)
@@ -84,17 +90,24 @@ def bridge() -> None:
     persistent_data: PersistentData = json.loads(persistent_data_store.read_text())
 
     while True:
-
         # avoid calling the API too often, otherwise the account might be banned
         # also makes sure to respect the interval if the process just restarted (e.g. in a bootloop)
-        if datetime.datetime.now().timestamp() - persistent_data["last_apple_api_call"] < POLLING_INTERVAL:
+        if (
+            datetime.datetime.now().timestamp() - persistent_data["last_apple_api_call"]
+            < POLLING_INTERVAL
+        ):
             # sleep short durations so that SIGTERM stops the container
             time.sleep(1)
         else:
-            already_uploaded = {(location["id"], location["timestamp"]) for location in persistent_data["uploaded_locations"]}
+            already_uploaded = {
+                (location["id"], location["timestamp"])
+                for location in persistent_data["uploaded_locations"]
+            }
 
             result = acc.fetch_last_reports(keys)
-            persistent_data["last_apple_api_call"] = int(datetime.datetime.now().timestamp())
+            persistent_data["last_apple_api_call"] = int(
+                datetime.datetime.now().timestamp()
+            )
 
             for key, reports in result.items():
                 # traccar expects unique int ids for each device
@@ -107,24 +120,24 @@ def bridge() -> None:
                     key.hashed_adv_key_b64,
                 )
                 for report in sorted(reports):
-
                     location = Location(
                         id=traccar_id,
                         lat=report.latitude,
                         lon=report.longitude,
-                        timestamp=int(report.timestamp.timestamp())
+                        timestamp=int(report.timestamp.timestamp()),
                     )
 
                     # duplicate check
                     if (location["id"], location["timestamp"]) not in already_uploaded:
-
                         resp = requests.post(
                             TRACCAR_SERVER,
                             data=location,
                         )
 
                         if resp.status_code == 200:
-                            already_uploaded.add((location["id"], location["timestamp"]))
+                            already_uploaded.add(
+                                (location["id"], location["timestamp"])
+                            )
                             persistent_data["uploaded_locations"].append(location)
                         elif resp.status_code == 400:
                             # device id has not been claimed yet in the traccar UI. remember to retry
